ca2d63dc3e81ee7abaac031e00f8365cdfdb95bd
Maurice Murphy <mauricemurphy@Maurices-MacBook-Pro.local>
2022-05-20 14:42:47 +0100

    Set up basic server and frontend, Make call to external api and display data on frontend

--- server/server.py
+++ server/server.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import requests
+from dotenv import load_dotenv
+import os
+
+api_key = os.getenv("NASA_API-KEY")
+
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/data")
+def get_data():
+    r = requests.get(f"https://eonet.gsfc.nasa.gov/api/v2.1/events?limit=5&days=20&source=InciWeb&status=open&api_key={api_key}")
+    print(r.status_code)
+    return r.json()
 
 if __name__ == '__main__':
     app.run(debug=True)
